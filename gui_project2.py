8fc585f95fba7b435ce879b0f8e3888373651a97
ASUS <pialkhanpial@gmail.com>
2023-06-18 22:35:25 +0600

    GUI done

--- gui_project2.py
+++ gui_project2.py
@@ -1,13 +1,15 @@
 import copy
 import math
+import random
+import time
 import tkinter as tk
-from tkinter import Button
 from pygame import mixer
 
 from tkinter import *
 
 depth = None
 difficulty = ""
+current_player = ""
 
 
 def checkWins():
@@ -37,7 +39,6 @@ def generate_moves(scores, selected):
 
 ############################# Heuristic ###################################
 def evaluate(aiScore, human1Score, human2Score):
-    # Calculates the total score for the selected scores
     return aiScore - (human1Score + human2Score)
 
 
@@ -75,7 +76,7 @@ def minimax(depth, alpha, beta, maximizing_player, scores, selected, aiScore, hu
                 human1Score += scores[move]
             else:
                 human2Score += scores[move]
-            eval_score = minimax(depth - 2, alpha, beta, (maximizing_player + 1) % 3, scores, selected, aiScore,
+            eval_score = minimax(depth - 1, alpha, beta, (maximizing_player + 1) % 3, scores, selected, aiScore,
                                  human1Score, human2Score)
             selected[move] = False
             if maximizing_player == 1:
@@ -186,9 +187,12 @@ def create_subtraction_buttons():
 
 def ai_turn():
     global current_player, humanTotalScore1, humanTotalScore2, aiTotalScore
+    current_player = "AI"
+    player_label.config(text="Current player: " + current_player)
+    window.update()
+    time.sleep(1)
     # messagebox.showinfo("AI Turn", "AI's turn!")
     best_score = float('-inf')
-    best_move = 0
     best_score_sub = float('-inf')
     best_move_sub = 0
     for move in generate_moves(game_state, selected):
@@ -224,27 +228,18 @@ def ai_turn():
     if game_over(selectedSub):
         checkWins()
     else:
-        current_player = "Player 1"
         player1_turn()
 
 
 def player1_turn():
     global current_player
-
-    # for button in score_buttons:
-    #     button.config(state=tk.NORMAL if not selected[button.index] else tk.DISABLED)
-    # for button in subtraction_buttons:
-    #     button.config(state=tk.NORMAL if not selectedSub[button.index] else tk.DISABLED)
+    current_player = "Player 1"
     player_label.config(text="Current player: " + current_player)
 
 
 def player2_turn():
     global current_player
-
-    for button in score_buttons:
-        button.config(state=tk.NORMAL if not selected[button.index] else tk.DISABLED)
-    for button in subtraction_buttons:
-        button.config(state=tk.NORMAL if not selectedSub[button.index] else tk.DISABLED)
+    current_player = "Player 2"
 
     player_label.config(text="Current player: " + current_player)
 
@@ -271,7 +266,6 @@ def process_subtraction_selection(index):
 
     if not selectedSub[index]:
         chosen_sub_index = index
-
         selectedSub[chosen_sub_index] = True
         score_sub = subtraction_state[chosen_sub_index]
         if current_player == "Player 1":
@@ -286,13 +280,14 @@ def process_subtraction_selection(index):
         update_scores()
         update_game_state()
         gameBounsSound()
+
         if game_over(selectedSub):
             checkWins()
         else:
             if current_player == 'Player 1':
-                current_player = 'Player 2'
                 player2_turn()
             else:
+                current_player = "AI"
                 ai_turn()
 
 
@@ -330,12 +325,14 @@ def gameOverSound():
 def save_difficulty(difficul):
     global depth
     global difficulty
+
     difficulty = difficul
 
     if difficulty == "Easy":
         depth = 0
     elif difficulty == "Medium":
-        depth = 1
+        # medium level
+        depth = math.log(len(score_array), 2) / 2
     else:
         depth = math.log(len(score_array), 2)
     difficulty_label = tk.Label(window, text="Level: " + difficulty, bg="#f8f9fa", fg="#495057",
@@ -346,20 +343,38 @@ def save_difficulty(difficul):
     buttonHard.destroy()
     buttonMedium.destroy()
     select_level.destroy()
-    create_score_buttons()
-    create_subtraction_buttons()
-    window.after(1000, ai_turn)
+
     print("Difficulty level selected:", difficulty)
 
 
-def create_button(btn, color):
+def save_current_player(cur_player):
+    # also check if difficulty is selected
+    global current_player
+    if difficulty != "":
+        buttonFirstPlayer.destroy()
+        buttonSecondPlayer.destroy()
+        buttonAI.destroy()
+        select_current_player.destroy()
+        current_player = cur_player
+        if current_player == "Player 1":
+            player1_turn()
+        elif current_player == "Player 2":
+            player2_turn()
+        else:
+            window.after(100, ai_turn)
+        print("Current player:", current_player)
+        create_score_buttons()
+        create_subtraction_buttons()
+
+
+def create_button(btn, color, paddingx):
     btn.config(
         bg=color,
         fg="white",
         font=("8514oem", 12),
         bd=1,
         relief="solid",
-        padx=10,
+        padx=paddingx,
         pady=5,
         width=4,
         height=1
@@ -367,7 +382,8 @@ def create_button(btn, color):
 
 
 ####################################################################### Initialize Game ###################################################################################################
-current_player = "AI"
+
+
 # Create the main window
 window = tk.Tk()
 window.title("Number Game")
@@ -418,10 +434,35 @@ score_buttons = []
 subtraction_buttons = []
 
 # Global variables
-score_array = [8, 12, -3, 17, -5, 6, -7, 19, 21]
-subtraction_array = [0, 2, 5, 27, -13, 6, 1, 8, -9]
+size = 9  # Adjust the size according to your needs
+min_value = -50
+max_value = 55
 
-######################################################## Select Label ##################################################
+score_array = [random.randint(min_value, max_value) for _ in range(size)]
+subtraction_array = [random.randint(min_value, max_value) for _ in range(size)]
+
+######################################################## Current player ##################################################
+select_current_player = tk.Label(window, text="Select first player", bg="white", fg="#2d3436",
+                                 font=("8514oem", 14, "bold"), padx=15, pady=8, bd=1,
+                                 relief="solid")
+select_current_player.place(relx=.5, rely=0.6, anchor="center")
+buttonFirstPlayer = Button(text=str("Player 1"),
+                           command=lambda: save_current_player("Player 1"))
+create_button(buttonFirstPlayer, "#27ae60", 25)
+buttonFirstPlayer.place(relx=.5 - .078, rely=.7, anchor="center")
+
+buttonSecondPlayer = Button(text=str("Player 2"),
+                            command=lambda: save_current_player("Player 2"))
+create_button(buttonSecondPlayer, "#f39c12", 25)
+buttonSecondPlayer.place(relx=.5, rely=.7, anchor="center")
+
+buttonAI = Button(text=str("AI"),
+                  command=lambda: save_current_player("AI"))
+create_button(buttonAI, "#e74c3c", 25)
+buttonAI.place(relx=.5 + .078, rely=.7, anchor="center")
+######################################################## Select Label  ##################################################
+
+
 select_level = tk.Label(window, text="Select Level", bg="white", fg="#2d3436",
                         font=("8514oem", 14, "bold"), padx=15, pady=8, bd=1,
                         relief="solid")
@@ -429,17 +470,17 @@ select_level.place(relx=.5, rely=0.40, anchor="center")
 
 buttonEasy = Button(text=str("Easy"),
                     command=lambda: save_difficulty("Easy"))
-create_button(buttonEasy, "#27ae60")
+create_button(buttonEasy, "#27ae60", 15)
 buttonEasy.place(relx=.5 - .078, rely=.5, anchor="center")
 
 buttonMedium = Button(text=str("Medium"),
                       command=lambda: save_difficulty("Medium"))
-create_button(buttonMedium, "#f39c12")
+create_button(buttonMedium, "#f39c12", 15)
 buttonMedium.place(relx=.5, rely=.5, anchor="center")
 
 buttonHard = Button(text=str("Hard"),
                     command=lambda: save_difficulty("Hard"))
-create_button(buttonHard, "#e74c3c")
+create_button(buttonHard, "#e74c3c", 15)
 buttonHard.place(relx=.5 + .078, rely=.5, anchor="center")
 ########################################################################################################################
 game_state = copy.deepcopy(score_array)
